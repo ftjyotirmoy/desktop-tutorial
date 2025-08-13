f2bce04769bc55697afea5aa7d8642c6ae4b9f9b
Jyotirmoy Deka <deka45227@gmail.com>
2025-08-13 20:22:05 +0530

    Update app.js

--- currency converter/app.js
+++ currency converter/app.js
@@ -7,6 +7,14 @@ const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg")
 
+
+
+
+
+
+
+
+
 for (let select of dropdowns) {
 for (currCode in countryList) {
 let newOption = document.createElement("option");
@@ -68,6 +76,7 @@ msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCur
 console.error("Error fetching or processing data: ", error);
 }
 });
+
 
 
 
