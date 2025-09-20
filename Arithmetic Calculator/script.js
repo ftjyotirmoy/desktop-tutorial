f48b430e5cda3a90b955d3242660cca343243f43
Jyotirmoy Deka <deka45227@gmail.com>
2025-09-20 15:12:24 +0530

    Update script.js

--- Arithmetic Calculator/script.js
+++ Arithmetic Calculator/script.js
@@ -2,6 +2,23 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator = null;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const screen = document.querySelector('.screen');
 
 function buttonClick(value) {
@@ -86,6 +103,7 @@ function init(){
 }
 
 init();
+
 
 
 
