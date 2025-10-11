3de486810f30caaa817b4fcb0129970c53039129
Jyotirmoy Deka <deka45227@gmail.com>
2025-10-11 22:25:17 +0530

    Update app.js

--- currency converter/app.js
+++ currency converter/app.js
@@ -20,8 +20,6 @@ const BASE_URL =
 
 
 
-
-
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
@@ -89,6 +87,7 @@ msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCur
 console.error("Error fetching or processing data: ", error);
 }
 });
+
 
 
 
