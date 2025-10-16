d066879058c87b8be26b7850a9608a969f83c0f6
Jyotirmoy Deka <deka45227@gmail.com>
2025-10-16 21:58:57 +0530

    Update app.js

--- currency converter/app.js
+++ currency converter/app.js
@@ -11,9 +11,6 @@ const msg = document.querySelector(".msg")
 
 
 
-
-
-
 for (let select of dropdowns) {
 for (currCode in countryList) {
 let newOption = document.createElement("option");
@@ -75,6 +72,7 @@ msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCur
 console.error("Error fetching or processing data: ", error);
 }
 });
+
 
 
 
